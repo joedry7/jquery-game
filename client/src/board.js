96bc92e83a870cab9d45b5a38b917d76ccd335f5
Joseph Dry <joe.dry6477@gmail.com>
2021-01-23 11:48:59 -0500

    Enemies rendering to page

--- client/src/board.js
+++ client/src/board.js
@@ -2,6 +2,14 @@ class Board {
   constructor(enemies) {
     this.$node = $('<div id="game-board"></div>');
     this.setBoard();
+    this.makeEnemies(enemies);
+  }
+
+  makeEnemies(n) {
+    let enemies = [];
+    for (let i = 0; i < n; ++i) {
+      enemies.push(new Enemy());
+    }
   }
 
   setBoard() {
@@ -9,7 +17,9 @@ class Board {
       height: '500px',
       width: '750px',
       margin: '50px auto',
-      border: 'blue 1px solid'
+      border: 'blue 1px solid',
+      position: 'relative',
+      overflow: 'hidden'
     }
 
     this.$node.css(style)
@@ -19,4 +29,4 @@ class Board {
 
 }
 
-$('document').ready(() => new Board());
+$('document').ready(() => new Board(10));
---
+++ client/src/enemy.js
@@ -0,0 +1,28 @@
+class Enemy {
+  constructor() {
+    this.$node = $(`<div class="enemy"></div>`);
+    this.setEnemy();
+  }
+
+  setEnemy() {
+    let style = {
+      height: '20px',
+      width: '20px',
+      borderRadius: '50%',
+      backgroundColor: 'black',
+      position: 'absolute',
+      top: '250px',
+      left: '375px'
+    }
+
+    this.$node.css(style)
+
+    $('#game-board').append(this.$node);
+    let position = {
+      top: `${Math.random() * 500}px`,
+      left: `${Math.random() * 750}px`
+    }
+    this.$node.animate(position, 'slow')
+  }
+
+}
